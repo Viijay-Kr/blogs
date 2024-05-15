6c6f25684b3e8544728c02af48378294671a2399
Viijay-Kr <vjkrishna689@gmail.com>
2024-05-15 10:08:04 +0200

    feat: integrate reactoid comments

--- src/components/EmbedReactToid.tsx
+++ src/components/EmbedReactToid.tsx
@@ -1,8 +1,15 @@
 import "@reacttoid/react/dist/reactoid.css";
-import { ReactToid, ReactoidContextProvider } from '@reacttoid/react';
+import { ReactToid, ReactoidContextProvider } from "@reacttoid/react";
 
 export const EmbedReactToid = () => {
-    return <ReactoidContextProvider clientId={import.meta.env.PUBLIC_REACTOID_APP_CLIENT_ID} clientSecret={import.meta.env.PUBLIC_REACTOID_APP_CLIENT_SECRET} slug={location.pathname} projectId={import.meta.env.PUBLIC_REACTOID_APP_PROJECT_ID}>
-        <ReactToid></ReactToid>
+  return (
+    <ReactoidContextProvider
+      clientId={import.meta.env.PUBLIC_REACTOID_APP_CLIENT_ID}
+      clientSecret={import.meta.env.PUBLIC_REACTOID_APP_CLIENT_SECRET}
+      slug={location.pathname}
+      projectId={import.meta.env.PUBLIC_REACTOID_APP_PROJECT_ID}
+    >
+      <ReactToid></ReactToid>
     </ReactoidContextProvider>
-} 
+  );
+};
